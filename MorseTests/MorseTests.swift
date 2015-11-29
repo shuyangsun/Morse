efa5b31b8d8177a9880b23dec8fa6a714f9f1362
Shuyang Sun <sunbuffett@gmail.com>
2015-11-29 16:00:12 -0600

    Initial Commit

---
+++ MorseTests/MorseTests.swift
@@ -0,0 +1,36 @@
+//
+//  MorseTests.swift
+//  MorseTests
+//
+//  Created by Shuyang Sun on 11/29/15.
+//  Copyright © 2015 Shuyang Sun. All rights reserved.
+//
+
+import XCTest
+@testable import Morse
+
+class MorseTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
